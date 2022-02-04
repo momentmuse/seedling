a2520c8aff1c9e859fd0976eea2395326f4e466f
Amy Kirasack <amykirasack@gmail.com>
2022-02-04 13:17:15 +0000

    fix: scroll on mobile & tablets. fixed build issues by adding --no-optimize flag (temp fix, it seems parcel has some known issues)

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroller = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
+  smartphone: { smooth: true },
+  tablet: { smooth: true },
 });
 
 gsap.registerPlugin(ScrollTrigger);
